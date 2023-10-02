019efc7501d04783c3b98c44ca93a52d557ea5ac
elavendanbuas <142242244+elavendanbuas@users.noreply.github.com>
2023-10-02 12:07:34 +0200

    Changes

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+module.exports = async function (context, req) {
+    context.res.json({
+        text: "Hello from the API"
+    });
+};
